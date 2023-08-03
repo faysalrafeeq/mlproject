142e991ff7f6dc303d9435c78c7ef706c56ffe2f
Faysal Rafeeq <faysalrafeeq12@gmail.com>
2023-08-03 12:52:40 -0700

    Model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -15,6 +15,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 # The dataclass hold the variables of a class and these variables are initialized without the 
 # Init method. This will serve as the input to my data ingestion.
 
@@ -57,4 +60,6 @@ if __name__ =='__main__':
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+    Modeltrainer = ModelTrainer()
+    print ( Modeltrainer.initiate_model_trainer(train_arr,test_arr) )
